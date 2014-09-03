955f1836b8d6fef20dfc8e130161a3bcfadece88
kujian <cbaojian@gmail.com>
2014-09-03 17:52:06 +0800

    fixed bug version 1.4

--- js/scrollfix.js
+++ js/scrollfix.js
@@ -1,5 +1,4 @@
-;
-(function($) {
+;(function($) {
 	//URI:http://caibaojian.com/scrollfix
 	//author:caibaojian
 	//website:http://caibaojian.com
@@ -22,51 +21,45 @@
 				documentHeight = $(document).height(), //文档高度
 				optsTop = opts.distanceTop, //定义到顶部的高度
 				outerHeight = obj.outerHeight(), //对象高度
-				marginBottom = (obj.css("marginBottom")).slice(0,-2),
-				outerHeight = parseInt(outerHeight) + parseInt(marginBottom),
+				outerHeight = parseFloat(outerHeight) - parseFloat(obj.css('marginTop').replace(/auto/, 0)),
 				outerWidth = obj.outerWidth(), //对象外宽度
 				objWidth = obj.width(),
 				startTop = $(opts.startTop), //开始浮动固定对象
 				startBottom = $(opts.startBottom),
 				toBottom, //停止滚动位置距离底部的高度
 				ScrollHeight, //对象滚动的高度
-				endfix; //开始停止固定的位置	
-			//如果没有找到节点，不进行处理
-			if(obj.length<=0){
-				return;
-			}
-			// 计算父节点的上边到顶部距离
-			// 如果 body 有 top 属性, 消除这些位移
-            var bodyToTop = parseInt(jQuery('body').css('top'), 10);
-            if(!isNaN(bodyToTop)) {
-                    optsTop += bodyToTop;
-            }
+				endfix; //开始停止固定的位置
 			if ($.isNumeric(opts.endPos)) {
 				toBottom = opts.endPos
 			} else {
 				toBottom = parseInt(documentHeight - $(opts.endPos).offset().top);
 			}
 			ScrollHeight = parseInt(documentHeight - toBottom), endfix = parseInt(ScrollHeight - outerHeight);
+			if (startTop[0]) {
+				var startTopOffset = startTop.offset(),
+					startTopPos = startTopOffset.top;
+				offsetTop = startTopPos;
+				// console.log(offsetTop);
+			}
+			if (startBottom[0]) {
+				var startBottomOffset = startBottom.offset(),
+					startBottomPos = startBottomOffset.top,
+					startBottomHeight = startBottom.outerHeight();
+				offsetTop = parseInt(startBottomPos + startBottomHeight);
+			}
+			//if ('undefined' != typeof(document.body.style.maxHeight)) {
+			toTop = parseInt(offsetTop - optsTop);
+			toTop = (toTop > 0) ? toTop : 0;
+			var selfBottom = documentHeight - offsetTop - outerHeight;
+			if(selfBottom<=endfix) return ;
 			//console.log(offsetTop);
 			$(window).scroll(function() {
 				var ScrollTop = $(window).scrollTop();
 				// console.log(start);
-				if (startTop[0]) {
-					var startTopOffset = startTop.offset(),
-						startTopPos = startTopOffset.top;
-					offsetTop = startTopPos;
-					//console.log(offsetTop);
-				}
-				if (startBottom[0]) {
-					var startBottomOffset = startBottom.offset(),
-						startBottomPos = startBottomOffset.top,
-						startBottomHeight = startBottom.outerHeight();
-					offsetTop = parseInt(startBottomPos + startBottomHeight);
-				}
-				//if ('undefined' != typeof(document.body.style.maxHeight)) {
-				toTop = parseInt(offsetTop - optsTop);
-				toTop = (toTop > 0) ? toTop : 0;
-				var selfBottom = documentHeight - offsetTop - outerHeight;
+
+				// console.log(ScrollTop);
+				// console.log(toTop);
+				// console.log(endfix);
 				//if(opts.startPos)
 				if ((ScrollTop > toTop) && (ScrollTop < endfix)) {
 					obj.fadeIn().css({
@@ -80,16 +73,12 @@
 						'height': outerHeight,
 						'_height': "0"
 					}).insertBefore(obj)
-				} else if ((ScrollTop >= endfix)&& (selfBottom<=endfix) ){
+				} else if (ScrollTop >= endfix) {
 					obj.css({
 						"position": "absolute",
 						"top": endfix,
 						"width": objWidth
-					});
-					placeholder.css({
-						'height': outerHeight,
-						'_height': "0"
-					}).insertBefore(obj)
+					})
 				} else {
 					obj.css({
 						"position": "static",
